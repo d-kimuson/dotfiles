1aee8f867936b3f537ec2d3b287163fa4a94b5e6
d-kimsuon <dev.kimuson@gmail.com>
2023-04-10 11:54:19 +0900

    feat(docker): lima は arm/x86 vm を共存できるようにする

--- .commands/docker.sh
+++ .commands/docker.sh
@@ -11,41 +11,93 @@ alias dcmp='docker-compose'
 # Functions
 # ====================
 
-function lima-status() {
-  if [ "$(limactl list | grep 'docker' | grep 'Running')" != "" ]; then
+function lima-status-x86() {
+  if [ "$(limactl list | grep 'docker-x86' | grep 'Running')" != "" ]; then
     echo "running"
-    elif [ "$(limactl list | grep 'docker' | grep 'Stopped')" != "" ]; then
+    elif [ "$(limactl list | grep 'docker-x86' | grep 'Stopped')" != "" ]; then
     echo "stopped"
   else
     echo "deleted"
   fi
 }
 
-function start-lima() {
-  LIMA_STATUS=$(lima-status)
+function lima-status-arm() {
+  if [ "$(limactl list | grep 'docker-arm' | grep 'Running')" != "" ]; then
+    echo "running"
+    elif [ "$(limactl list | grep 'docker-arm' | grep 'Stopped')" != "" ]; then
+    echo "stopped"
+  else
+    echo "deleted"
+  fi
+}
+
+function start-lima-x86() {
+  LIMA_STATUS=$(lima-status-x86)
   if [ "$LIMA_STATUS" = "running" ]; then
     echo "lima is already runninng."
     elif [ "$LIMA_STATUS" = "stopped" ]; then
-    cd ~/dotfiles/config/lima && limactl start docker && cd -
+    cd ~/dotfiles/config/lima && limactl start docker-x86 && cd -
   else
-    cd ~/dotfiles/config/lima && limactl start ./docker.yaml && cd -
+    cd ~/dotfiles/config/lima && limactl start ./docker-x86.yaml && cd -
   fi
+}
+
+function start-lima-arm() {
+  LIMA_STATUS=$(lima-status-arm)
+  if [ "$LIMA_STATUS" = "running" ]; then
+    echo "lima is already runninng."
+    elif [ "$LIMA_STATUS" = "stopped" ]; then
+    cd ~/dotfiles/config/lima && limactl start docker-arm && cd -
+  else
+    cd ~/dotfiles/config/lima && limactl start ./docker-arm.yaml && cd -
+  fi
+}
+
+function start-lima() {
+  start-lima-x86
+  start-lima-arm
+}
+
+function stop-lima-x86() {
+  # 現状わざわざ stop するのは壊れて再起動したいときくらいなので削除までまとめちゃう
+  LIMA_STATUS=$(lima-status-x86)
+  
+  if [ "$LIMA_STATUS" = "running" ]; then
+    limactl stop -f docker-x86
+  fi
+  
+  limactl delete docker-x86
+}
+
+function stop-lima-arm() {
+  # 現状わざわざ stop するのは壊れて再起動したいときくらいなので削除までまとめちゃう
+  LIMA_STATUS=$(lima-status-arm)
+  
+  if [ "$LIMA_STATUS" = "running" ]; then
+    limactl stop -f docker-arm
+  fi
+  
+  limactl delete docker-arm
 }
 
 function stop-lima() {
-  # 現状わざわざ stop するのは壊れて再起動したいときくらいなので削除までまとめちゃう
-  LIMA_STATUS=$(lima-status)
-  
-  if [ "$LIMA_STATUS" = "running" ]; then
-    limactl stop -f docker
-  fi
-  
-  limactl delete docker
+  stop-lima-x86
+  stop-lima-arm
+}
+
+function restart-lima-x86() {
+  stop-lima-x86
+  start-lima-x86
+}
+
+function restart-lima-arm() {
+  stop-lima-arm
+  start-lima-arm
 }
 
 function restart-lima() {
-  stop-lima
-  start-lima
+  restart-lima-x86
+  restart-lima-arm
 }
 
 function dkill () {
