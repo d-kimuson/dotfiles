b67e7ad006e72e51965f9b6f6a3b178948db6654
d-kimsuon <dev.kimuson@gmail.com>
2025-10-23 11:11:16 +0900

    modular-mcp に載せ替える

--- scripts/claude_code_mcp.sh
+++ scripts/claude_code_mcp.sh
@@ -3,5 +3,4 @@
 set -uo pipefail
 
 # 存在したらエラーになっちゃうが冪等にしたいので set +e して /dev/null に捨てる
-~/.claude/local/claude mcp add context7 -s user -- npx -y @upstash/context7-mcp@latest > /dev/null 2>&1
-~/.claude/local/claude mcp add serena -s user -- uvx --from git+https://github.com/oraios/serena serena start-mcp-server > /dev/null 2>&1
+~/bin/claude mcp add modular-mcp -s user -- npx -y @kimuson/modular-mcp@latest $(git rev-parse --show-toplevel)/modular-mcp.json > /dev/null 2>&1
